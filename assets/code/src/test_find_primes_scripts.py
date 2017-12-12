9c62643bd9a4937c1566efb553948ff5e36d88ed
Vince Knight <vincent.knight@gmail.com>
2017-12-12 11:52:30 +0000

    Fix links to Python scripts.
    Closes #43

--- assets/code/src/test_find_primes_scripts.py
+++ assets/code/src/test_find_primes_scripts.py
@@ -7,16 +7,16 @@ expected_out = b'[(2, 3), (11, 1), (23, 1)]\n[(7, 1)]\n'
 cwd = "./assets/code/src/"  # TODO This means it can only be run from root dir
 
 def test_01_find_primes():
-    out = subprocess.check_output(["python", "01-find_primes.py"],
+    out = subprocess.check_output(["python", "find_primes_v1.py"],
                                   cwd=cwd)
     assert out == expected_out
 
 def test_02_find_primes():
-    out = subprocess.check_output(["python", "02-find_primes.py"],
+    out = subprocess.check_output(["python", "find_primes_v2.py"],
                                   cwd=cwd)
     assert out == expected_out
 
 def test_03_find_primes():
-    out = subprocess.check_output(["python", "02-find_primes.py"],
+    out = subprocess.check_output(["python", "find_primes_v3.py"],
                                   cwd=cwd)
     assert out == expected_out
